d667ac3ee524bd8c275aa213d9a03786ca0aec29
Mahalakshmi Ganesan <88181302+19maha@users.noreply.github.com>
2021-09-19 09:46:09 -0600

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="navbar.css">
+</head>
+<div class="navbar">
+       <img src="./photos/icon.png" alt="logo" height="50px" width="50px">
+       <p style="font-size:40px;padding-left: 10px;padding-top: 5px">SPARKS BANK</p>
+       <nav>
+         <ul>
+           <li><a href="index.php">Home</a></li>
+         </ul>
+       </nav>
+</div>
+</html>
